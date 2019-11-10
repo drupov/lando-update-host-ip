77d395710a9876e9da7e43621d72316464f61825
Dimitar Rupov <dimitar@precessionmedia.com>
2019-11-10 01:07:32 +0100

    Add script

---
+++ lando-update-host-ip.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+# Get the host ip - the one that changed after the network switch.
+hostip=`lando config --path appEnv.LANDO_HOST_IP`
+# Strip the quotes from the output.
+hostip=`echo $hostip | cut -d "'" -f 2`
+
+# Get the current value of the LANDO_HOST_IP within the contaner.
+containerip=`lando ssh -c "printenv LANDO_HOST_IP"`
+# Remove the last character from the received value.
+containerip=${containerip%?}
+
+# Stop the docker service.
+sudo service docker stop
+# Replace the IP with the new one for all docker containers.
+sudo find /var/lib/docker/containers -type f -name "*" -exec sed -i'' -e "s/$containerip/$hostip/g" {} +
+# Start the docker service.
+sudo service docker start
+# Restart the app after the docker restart.
+lando restart
